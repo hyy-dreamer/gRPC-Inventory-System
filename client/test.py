b057163933501069c1a080d88d6e9083a10e9a19
hyy-dreamer <yuyanghu@andrew.cmu.edu>
2022-12-13 15:36:37 -0500

    add mock test

---
+++ client/test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase, mock
+import unittest
+import Inventory_client
+from get_book_titles import Get_Book_Titles
+
+def fakeGetBook(self, id):
+    class object:
+        title = "We are testing"
+    return object
+    
+class test(TestCase):
+    @mock.patch.object(Inventory_client.InventoryClient, 'GetBook', fakeGetBook)
+    def test_get_book_titles(self):
+        getbook = Get_Book_Titles()
+        ISBNs = ["1"]
+        self.assertEqual(getbook.GetBookTilte(ISBNs), ["We are testing"])
+if __name__ == "__main__":
+    unittest.main()
